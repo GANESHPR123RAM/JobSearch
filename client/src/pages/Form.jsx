81ecf7549c0fa20d9aad90adcb6a46ab8ecf71f5
ganesh prajapati <129645178+GANESHPR123RAM@users.noreply.github.com>
2024-08-31 23:10:37 +0530

    Update Form.jsx

--- client/src/pages/Form.jsx
+++ client/src/pages/Form.jsx
@@ -39,7 +39,7 @@ const handleSubmit = async (e) => {
   formDataWithFile.append("resume", formData.resume);  // Handle file upload
 
   try {
-      const response = await fetch(`http://localhost:5000/api/form`, {
+      const response = await fetch(`https://jobsearch-server-g3bl.onrender.com/api/form`, {
           method: "POST",
           body: formDataWithFile,  // Send FormData directly
       });
