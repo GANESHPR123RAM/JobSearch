49e58a26c1ca37b8ae38462afb5901c18597c414
ganesh prajapati <129645178+GANESHPR123RAM@users.noreply.github.com>
2024-08-31 23:11:19 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -23,7 +23,7 @@ export const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch('http://localhost:5000/api/auth/login', {
+            const response = await fetch('https://jobsearch-server-g3bl.onrender.com/api/auth/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
